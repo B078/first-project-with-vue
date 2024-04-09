d70642c7a1107211a02901b14c4d9e0cfd1cf888
B078 <146114708+B078@users.noreply.github.com>
2024-04-10 01:40:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'my first page with Vue :)'
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  color: blue;
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
